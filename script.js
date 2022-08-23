f21357acc4556826fa05993d269a2a1f8ffef4a3
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-08-23 10:46:50 +0300

    event listener

---
+++ script.js
@@ -0,0 +1,9 @@
+const form= document.getElementById("form")
+alert("Fill form")
+form.addEventListener('submit',(e) =>{
+    e.preventDefault;
+    const message = document.getElementsByClassName('sub_container')
+    const intro = document.getElementsById('intro').value;
+    // document.getElementByClassName("intro").innerHTML(getElementById("form").value)
+    console.log(getElementsById('intro'))}
+)
